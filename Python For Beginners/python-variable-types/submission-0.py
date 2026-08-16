3fb74ccc04a907f4df07efdae5dafcd49b462290
Himanshu Girotra Chakraborty <chakraborty.himanshu@gmail.com>
2026-08-16 16:36:55 +0530

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,12 @@
+integer_type = 3
+float_type = 5.0
+bool_type = True
+string_type = "abc"
+list_type = [3, 5, "hello"]
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
